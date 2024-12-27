d99a98ddd993be31e5558c7199047161372cac4e
arman <armangh1212@gmail.com>
2024-12-27 16:39:18 +0330

    test

--- frontend/src/pages/Friendly/index.tsx
+++ frontend/src/pages/Friendly/index.tsx
@@ -9,21 +9,33 @@ const Friendly = () => {
   return (
     <>
       <Button className="" label="Create" variant={"secondary"} />
-      <div className="flex w-dvw" >
-        <div className="m-auto p-auto h-full w-1/2 border-dotted p-4  border-2">
-          <div className="grid gap-5 w-72" >
-            <div className="my-4 flex justify-between">
+      <div className="flex" >
+        <div className="m-auto p-auto w-1/2 border-dotted p-4  border-2">
+          <div className="flex gap-5 justify-around" >
+            <div className="my-4 grid grid-cols-1 gap-4 ">
               <span className="p-4 bg-orange-hover text-white " >
                 User 1
               </span  >
               <span className="p-4 bg-orange-hover text-white">
                 User 2
               </span>
-            </div>
-            <div className="mt-4 flex justify-between" >
-              <span className="p-4 bg-orange-hover text-white" >
+              <span className="p-4 bg-orange-hover text-white">
                 User 3
+              </span>
+              <span className="p-4 bg-orange-hover text-white">
+                User 4
+              </span>
+            </div>
+            <div className="my-4 grid grid-cols-1 gap-4 justify-between">
+              <span className="p-4 bg-orange-hover text-white " >
+                User 1
               </span  >
+              <span className="p-4 bg-orange-hover text-white">
+                User 2
+              </span>
+              <span className="p-4 bg-orange-hover text-white">
+                User 3
+              </span>
               <span className="p-4 bg-orange-hover text-white">
                 User 4
               </span>
@@ -31,19 +43,18 @@ const Friendly = () => {
           </div>
         </div>
         <div className="w-1/2">
-          <div className="ms-4 mb-2 items-center" >
+          <div className="flex ms-4 mb-2 items-center" >
             <Input
-              className="col-span-1"
               value={searchingLoby}
               onChange={(e: any) => setSearchingLoby(e.target.value)}
               placeholder="Search . . ."
             />
-            <span>
+            <span className="" >
               <SearchIcon className="text-gray-800 ms-2 " />
             </span>
           </div>
-          <div className=" mx-5 w-96" >
-            <div className=" justify-between bg-gray-100 p-4">
+          <div className="bg-red mx-5 w-96" >
+            <div className="overflow-scroll min-h-10 max-h-96 justify-between bg-gray-100 p-4">
               <div className="border-b my-2 p-2 grid gap-3 grid-cols-2" >
                 <span className=" " >
                   1. bad_room
@@ -58,6 +69,95 @@ const Friendly = () => {
                 </span>
                 <span className="text-gray-900 flex justify-end">
                   6 player
+                </span>
+              </div>
+              <div className="border-b my-2 p-2 grid gap-3 grid-cols-2" >
+                <span className=" " >
+                  3. bad_room
+                </span>
+                <span className="text-gray-900 flex justify-end">
+                  3 player
+                </span>
+              </div>
+              <div className="border-b my-2 p-2 grid gap-3 grid-cols-2" >
+                <span className=" " >
+                  3. bad_room
+                </span>
+                <span className="text-gray-900 flex justify-end">
+                  3 player
+                </span>
+              </div>
+              <div className="border-b my-2 p-2 grid gap-3 grid-cols-2" >
+                <span className=" " >
+                  3. bad_room
+                </span>
+                <span className="text-gray-900 flex justify-end">
+                  3 player
+                </span>
+              </div>
+              <div className="border-b my-2 p-2 grid gap-3 grid-cols-2" >
+                <span className=" " >
+                  3. bad_room
+                </span>
+                <span className="text-gray-900 flex justify-end">
+                  3 player
+                </span>
+              </div>
+              <div className="border-b my-2 p-2 grid gap-3 grid-cols-2" >
+                <span className=" " >
+                  4. bad_room
+                </span>
+                <span className="text-gray-900 flex justify-end">
+                  2 player
+                </span>
+              </div>
+              <div className="border-b my-2 p-2 grid gap-3 grid-cols-2" >
+                <span className=" " >
+                  5. bad_room
+                </span>
+                <span className="text-gray-900 flex justify-end">
+                  2 player
+                </span>
+
+              </div>
+              <div className="border-b my-2 p-2 grid gap-3 grid-cols-2" >
+                <span className=" " >
+                  1. bad_room
+                </span>
+                <span className="text-gray-900 flex justify-end">
+                  4 player
+                </span>
+              </div>
+              <div className="border-b my-2 p-2 grid gap-3 grid-cols-2" >
+                <span className=" " >
+                  2. bad_room
+                </span>
+                <span className="text-gray-900 flex justify-end">
+                  6 player
+                </span>
+              </div>
+              <div className="border-b my-2 p-2 grid gap-3 grid-cols-2" >
+                <span className=" " >
+                  3. bad_room
+                </span>
+                <span className="text-gray-900 flex justify-end">
+                  3 player
+                </span>
+              </div>
+              <div className="border-b my-2 p-2 grid gap-3 grid-cols-2" >
+                <span className=" " >
+                  3. bad_room
+                </span>
+                <span className="text-gray-900 flex justify-end">
+                  3 player
+                </span>
+              </div>
+              <div className="border-b my-2 p-2 grid gap-3 grid-cols-2" >
+                <span className=" " >
+                  3. bad_room
+                </span>
+                <span className="text-gray-900 flex justify-end">
+                  3 player
                 </span>
               </div>
               <div className="border-b my-2 p-2 grid gap-3 grid-cols-2" >
