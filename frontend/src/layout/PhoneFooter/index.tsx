9428d63a539a9f4506411266b03f39694a06e315
arman <armangh131313@gmail.com>
2025-01-28 22:20:25 +0330

    css and swapper

--- frontend/src/layout/PhoneFooter/index.tsx
+++ frontend/src/layout/PhoneFooter/index.tsx
@@ -1,24 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import ResponsiveMaker from "../../utils/helpers/ResponsiveMaker";
 import HomeIcon from "@mui/icons-material/Home";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import LogoTC from "../../assets/img/1724181984017.jpg";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import NotificationsIcon from "@mui/icons-material/Notifications";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { cn } from "../../utils/tw-utils";
 
 const PhoneFooter: React.FC = () => {
+  const [activeIcon, setActiveIcon] = useState<string | null>(null); // وضعیت آیکون فعال
+
+  const handleWatchClick = () => {
+    setActiveIcon("watch"); // تنظیم آیکون Watch به عنوان فعال
+  };
+
   return (
-    <ResponsiveMaker hiddenWidth={768}>
-      <div className="sticky bottom-0 left-0 w-full bg-white shadow-card p-2 text-center">
+    <ResponsiveMaker visibleWidth={400}>
+      <div className="sticky bottom-0 left-0 w-full bg-white shadow-card text-center">
         <div className="flex gap-10 justify-center">
-          <Link to={"/home"}>
-            <HomeIcon className=" font30 text-orange-hover" />
-          </Link>
-          <Link to={"/watch"}>
-            <PlayArrowIcon className=" font30 text-orange-hover" />
-          </Link>
-          <Link to={"/sot"}>
+          <NavLink
+            to={"/home"}
+            className={({ isActive }) =>
+              cn(
+                "flex w-14 py-2 items-center  justify-center",
+                isActive ? " border-t-2 border-green" : ""
+              )
+            }>
+            <HomeIcon className="font30 text-orange-hover" />
+          </NavLink>
+          <NavLink
+            to={"/watch"}
+            onClick={handleWatchClick}
+            className={({ isActive }) =>
+              cn(
+                "flex w-14 py-2 items-center  justify-center",
+                isActive ? " border-t-2 border-green" : ""
+              )
+            }>
+            <PlayArrowIcon className={`font30 text-orange-hover `} />
+          </NavLink>
+          <NavLink
+            to={"/sot"}
+            className={({ isActive }) =>
+              cn(
+                "flex w-14 py-2 items-center  justify-center",
+                isActive ? " border-t-2 border-green" : ""
+              )
+            }>
             <img
               src={LogoTC}
               alt="Logo"
@@ -26,13 +55,27 @@ const PhoneFooter: React.FC = () => {
               width={30}
               height={30}
             />
-          </Link>
-          <Link to={"/notification"}>
-            <NotificationsIcon className=" font30 text-orange-hover" />
-          </Link>
-          <Link to={"/profile"}>
-            <AccountCircleIcon className=" font30 text-orange-hover" />
-          </Link>
+          </NavLink>
+          <NavLink
+            to={"/notification"}
+            className={({ isActive }) =>
+              cn(
+                "flex w-14 py-2 items-center  justify-center",
+                isActive ? " border-t-2 border-green" : ""
+              )
+            }>
+            <NotificationsIcon className="font30 text-orange-hover" />
+          </NavLink>
+          <NavLink
+            to={"/profile"}
+            className={({ isActive }) =>
+              cn(
+                "flex w-14 py-2 items-center  justify-center",
+                isActive ? "border-t-2 border-green" : ""
+              )
+            }>
+            <AccountCircleIcon className="font30 text-orange-hover" />
+          </NavLink>
         </div>
       </div>
     </ResponsiveMaker>
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -6,10 +6,16 @@ import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import ReactPlayer from "react-player";
 import CropFreeIcon from "@mui/icons-material/CropFree";
-// import demoVid from "../../../../../u-35446-1731760254601.mp4";
+import demoVid1 from "../../../../../d55c684301ec7e7fe086680c18c5e82d61899342-720p.mp4";
+import demoVid2 from "../../../../../VID_20230630_182952.mp4";
 import Comments from "../../common/Comments";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Mousewheel, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import ResponsiveMaker from "../../utils/helpers/ResponsiveMaker";
 
-const profiles = [
+const dubleVideos = [
   {
     img: "https://writestylesonline.com/wp-content/uploads/2018/11/Three-Statistics-That-Will-Make-You-Rethink-Your-Professional-Profile-Picture-1024x1024.jpg",
     rank: "https://cdn3d.iconscout.com/3d/premium/thumb/first-rank-badge-3d-icon-download-in-png-blend-fbx-gltf-file-formats--gold-medal-tag-reward-and-badges-pack-team-sports-icons-6878280.png?f=webp",
@@ -143,129 +149,212 @@ const Home = () => {
   };
 
   return (
-    <div className="col-span-12 justify-center flex md:col-span-12 lg:col-span-12">
-      <div className=" grid grid-cols-1 gap-8 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-        {profiles.map((profile, index) => (
-          <section
-            key={index}
-            className={`   ${expandedVideo === index ? "col-span-full" : ""}`}
-          >
-            <div
-              className={`relative ${
-                expandedVideo === index
-                  ? "w-full h-screen"
-                  : "w-full sm:w-[100%] md:w-[100%] lg:w-[325px] h-[650px]"
-              }transition-all duration-500`}
-            >
-              <div className="ms-1 absolute justify-between items-center m-1 text-2xl">
-                <div className="grid grid-cols-5  gap-2 my-1 items-center">
-                  <div className="relative mb-2  col-span-1">
-                    <img
-                      className="rounded-full relative"
-                      src={profile.img}
-                      width={60}
-                      height={60}
-                      alt="Profile"
-                    />
-                    <img
-                      className="absolute bottom-0"
-                      src={profile.rank}
-                      width={25}
-                      height={25}
-                      alt="Rank"
-                    />
+    <>
+      <ResponsiveMaker visibleWidth={600}>
+        <div className=" ">
+          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 justify-center  ">
+            {dubleVideos.map((profile, index) => (
+              <section key={index} className="">
+                <div className="ms-1 absolute justify-between items-center m-1 text-2xl  ">
+                  <div className="grid grid-cols-5 gap-2 my-1 items-center">
+                    <div className="relative mb-2 col-span-1">
+                      <img
+                        className="rounded-full relative"
+                        src={profile.img}
+                        width={50}
+                        height={50}
+                        alt="Profile"
+                      />
+                      <img
+                        className="absolute right-0 bottom-0"
+                        src={profile.rank}
+                        width={20}
+                        height={20}
+                        alt="Rank"
+                      />
+                    </div>
+                    <div className="flex col-span-2 items-start justify-start align-top">
+                      <span className="font-bold flex items-start text-white">
+                        {profile.username}
+                      </span>
+                    </div>
                   </div>
-                  <div className="flex col-span-2 items-start justify-start align-top ">
-                    <span className="font-bold flex items-start  text-white">
-                      {profile.username}
-                    </span>
-                    {/* {!!profile.cupPro && (
-                      <div className="flex items-end mx-2">
-                        <img width={25} height={25} src={profile.cupPro} />
+                </div>
+                <div className="absolute font35 top-2 right-4">
+                  <div className="flex flex-col gap-4 items-center">
+                    <div>
+                      {profile.type.toLowerCase() === "success" ? (
+                        <>
+                          <VerifiedIcon
+                            className={`flex items-center text-${profile.color} font35`}
+                          />
+                          <span className="text-green font18 font-bold">
+                            {profile.type}
+                          </span>
+                        </>
+                      ) : (
+                        <>
+                          <DangerousIcon className="flex items-center text-red font35" />
+                          <span className="text-red font18 font-bold">
+                            {profile.type}
+                          </span>
+                        </>
+                      )}
+                    </div>
+                    <div className="flex items-center gap-1 text-white">
+                      <ThumbUpOffAltIcon className="font20 font-bold" />
+                      <span>{profile.comments}</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-screen lg:w-full min-h-10 max-h-full flex flex-col justify-center   items-center p-2">
+                  <div className="w-full flex flex-col justify-between items-center mb-2">
+                    <div>
+                      <ReactPlayer
+                        controls
+                        width="100%"
+                        height="100%"
+                        url={demoVid1}
+                      />
+                    </div>
+                    <div>
+                      <ReactPlayer
+                        controls
+                        width="100%"
+                        height="100%"
+                        url={demoVid2}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="flex items-center gap-1 m-2">
+                  <span onClick={handleShowCMT}>
+                    <ChatBubbleOutlineIcon className="text-black font20 cursor-pointer" />
+                  </span>
+                  <span> 1,529 </span>
+                </div>
+                <p className="mx-2 text-white">
+                  this is gonna be the best video in the world that you can show
+                  your country. Don't forget to be the best!
+                </p>
+              </section>
+            ))}
+          </div>
+          {showComments && (
+            <Comments
+              handleShowCMT={handleShowCMT}
+              closingComments={closingComments}
+            />
+          )}
+        </div>
+      </ResponsiveMaker>
+      <ResponsiveMaker visibleWidth={400}>
+        <Swiper
+          direction={"vertical"}
+          slidesPerView={1}
+          spaceBetween={30}
+          mousewheel={true}
+          pagination={{ clickable: true }}
+          modules={[Mousewheel]}
+          className="mySwiper h-screen">
+          <div className="flex justify-center items-center h-full">
+            <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3">
+              {dubleVideos.map((profile, index) => (
+                <SwiperSlide
+                  key={index}
+                  className="flex flex-col w-full h-[50vh] bg-black">
+                  <div className="ms-1 absolute justify-between items-center m-1 text-2xl">
+                    <div className="grid grid-cols-5 gap-2 my-1 items-center">
+                      <div className="relative mb-2 col-span-1">
+                        <img
+                          className="rounded-full relative"
+                          src={profile.img}
+                          width={60}
+                          height={60}
+                          alt="Profile"
+                        />
+                        <img
+                          className="absolute bottom-0"
+                          src={profile.rank}
+                          width={25}
+                          height={25}
+                          alt="Rank"
+                        />
                       </div>
-                    )} */}
-                  </div>
-                </div>
-              </div>
-              <div className="absolute font35 top-2 right-4">
-                <div className="flex flex-col gap-4 items-center">
-                  <div>
-                    {profile?.type?.toLowerCase() === "success" ? (
-                      <>
-                        <VerifiedIcon
-                          className={`flex items-center text-${profile.color} font35`}
-                        />
-                        <span className="text-green font18 font-bold">
-                          {profile.type}
+                      <div className="flex col-span-2 items-start justify-start align-top">
+                        <span className="font-bold flex items-start text-white">
+                          {profile.username}
                         </span>
-                      </>
-                    ) : (
-                      <>
-                        <DangerousIcon
-                          className={`flex items-center text-red font35`}
-                        />
-                        <span className="text-red font18 font-bold">
-                          {profile.type}
-                        </span>
-                      </>
-                    )}
+                      </div>
+                    </div>
                   </div>
-                  <div className="flex text-white items-center gap-1">
-                    <ThumbUpOffAltIcon className=" font20 font-bold" />
-                    <span>{profile.comments}</span>
+                  <div className="absolute font35 top-2 right-4">
+                    <div className="flex flex-col gap-4 items-center">
+                      <div>
+                        {profile.type.toLowerCase() === "success" ? (
+                          <>
+                            <VerifiedIcon
+                              className={`flex items-center text-${profile.color} font35`}
+                            />
+                            <span className="text-green font18 font-bold">
+                              {profile.type}
+                            </span>
+                          </>
+                        ) : (
+                          <>
+                            <DangerousIcon className="flex items-center text-red font35" />
+                            <span className="text-red font18 font-bold">
+                              {profile.type}
+                            </span>
+                          </>
+                        )}
+                      </div>
+                      <div className="flex items-center gap-1 text-white">
+                        <ThumbUpOffAltIcon className="font20 font-bold" />
+                        <span>{profile.comments}</span>
+                      </div>
+                    </div>
                   </div>
-                  {/* <div
-                    className="flex justify-end  items-center "
-                    onClick={() => handleExpand(index)}>
-                    <CropFreeIcon
-                      className={` ${
-                        expandedVideo === index
-                          ? "text-green-500"
-                          : "text-white"
-                      } font35 cursor-pointer`}
-                    />
-                  </div> */}
-                </div>
-              </div>
-              <div className="w-full h-full bg-black flex flex-col justify-center items-center">
-                <div className="w-full h-[450px] flex justify-center items-center mb-2">
-                  <ReactPlayer
-                    controls
-                    width="100%"
-                    height="100%"
-                    // url={demoVid}
-                  />
-                </div>
-                <div className="w-full h-[450px] flex justify-center items-center">
-                  <ReactPlayer
-                    controls
-                    width="100%"
-                    height="100%"
-                    // url={demoVid}
-                  />
-                </div>
-              </div>
-              <div className="flex items-center gap-1 m-2">
-                <span onClick={handleShowCMT}>
-                  <ChatBubbleOutlineIcon className="text-black font20  cursor-pointer" />
-                </span>
-                <span> 1,529 </span>
-              </div>
-              <p className="mx-2">
-                this is ganna be best video in the world that u can whole your
-                country. dont forget to be best
-              </p>
+                  <div className="flex flex-col justify-center items-center w-full h-full p-2">
+                    <div className="flex flex-col justify-center items-center h-full">
+                      <ReactPlayer
+                        controls
+                        width="100%"
+                        height="100%"
+                        url={demoVid1}
+                        className="mb-2"
+                      />
+                      <ReactPlayer
+                        controls
+                        width="100%"
+                        height="100%"
+                        url={demoVid2}
+                      />
+                    </div>
+                  </div>
+                  <div className="flex items-center gap-1 m-2">
+                    <span onClick={handleShowCMT}>
+                      <ChatBubbleOutlineIcon className="text-white font20 cursor-pointer" />
+                    </span>
+                    <span>1,529</span>
+                  </div>
+                  <p className="mx-2 text-white">
+                    this is gonna be the best video in the world that you can
+                    show your country. Don't forget to be the best!
+                  </p>
+                </SwiperSlide>
+              ))}
             </div>
-          </section>
-        ))}
-      </div>
-      {showComments && (
-        <Comments
-          handleShowCMT={handleShowCMT}
-          closingComments={closingComments}
-        />
-      )}
-    </div>
+          </div>
+          {showComments && (
+            <Comments
+              handleShowCMT={handleShowCMT}
+              closingComments={closingComments}
+            />
+          )}
+        </Swiper>
+      </ResponsiveMaker>
+    </>
   );
 };
 
