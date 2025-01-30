c78abd9b75fffe79f422bcd0b860b4a7422d08e7
arman <armangh1212@gmail.com>
2025-01-30 12:32:44 +0330

    fix: rank image

--- frontend/src/components/Rank/index.tsx
+++ frontend/src/components/Rank/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import starRank from "../../assets/img/rank1.avif";
 import goldPer1 from "../../assets/img/rank10.webp";
+import goldStar2 from "../../assets/img/goldStar.webp";
+
 import bronze3 from "../../assets/img/rank11.webp";
 import gold1 from "../../assets/img/rank3.webp";
 import blue4 from "../../assets/img/rank4.png";
@@ -12,39 +14,44 @@ import silverPer2 from "../../assets/img/rank9.webp";
 import silverSircle2 from "../../assets/img/silverRank2.jpg";
 
 const Rank: React.FC = ({
-  starWidth = 15,
-  starHeight = 15,
+  starWidth = 10,
+  starHeight = 10,
   rankWidth = 50,
   rankHeight = 50,
 }: any) => {
   return (
-    <span className="bg-red">
-      <img width={rankWidth} height={rankHeight} src={goldPer1} alt="My Rank" />
-      <span className="flex mt-1 bg-blue gap-1">
+    <div className="bg-red flex">
+      <img
+        className=""
+        width={rankWidth}
+        height={rankHeight}
+        src={goldPer1}
+        alt="My Rank"
+      />
+      <span className="flex bg-orange-hover justify-center gap-1">
         <img
           className="rounded-full shadow-lg"
           width={starWidth}
           height={starHeight}
-          src={starRank}
+          src={goldStar2}
           alt="My Rank"
         />
         <img
           className="rounded-full shadow-lg"
           width={starWidth}
           height={starHeight}
-          src={starRank}
+          src={goldStar2}
           alt="My Rank"
         />
         <img
           className="rounded-full shadow-lg"
           width={starWidth}
           height={starHeight}
-          
-          src={starRank}
+          src={goldStar2}
           alt="My Rank"
         />
       </span>
-    </span>
+    </div>
   );
 };
 
