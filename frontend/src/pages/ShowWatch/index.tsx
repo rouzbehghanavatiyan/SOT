9d1d2794c3e59e8a258a2de27eeddaba8ed2b4c1
arman <armangh1212@gmail.com>
2025-02-09 09:50:51 +0330

    blob 2

--- frontend/src/pages/ShowWatch/index.tsx
+++ frontend/src/pages/ShowWatch/index.tsx
@@ -2,7 +2,10 @@ import { useEffect, useState } from "react";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { attachmentListByInviteId } from "../../services/dotNet";
+import {
+  attachmentListByInviteId,
+  attachmentPlay,
+} from "../../services/dotNet";
 import asyncWrapper from "../../common/AsyncWrapper";
 import Video from "../../components/Video";
 import { useAppSelector } from "../../hooks/hook";
@@ -10,6 +13,7 @@ const baseURL: string | undefined = import.meta.env.VITE_SERVERTEST;
 
 const ShowWatch = ({}) => {
   const [videoSrc, setVideoSrc] = useState([]);
+  const [dataBinery, setDataBinery] = useState(null);
   const { main } = useAppSelector((state) => state);
   const [isPlayingBottom, setIsPlayingBottom] = useState(false);
   const [isPlayingTop, setIsPlayingTop] = useState(false);
@@ -26,7 +30,14 @@ const ShowWatch = ({}) => {
     const res = await attachmentListByInviteId(inviteId);
     const { status, data } = res?.data;
     if (status === 0) {
-      setVideoSrc(res?.data?.data);
+      const fixVideo1 = `wwwroot/${data[0]?.attachmentType}/${data[0]?.fileName}${data[0]?.ext}`;
+      console.log("1", data);
+      setVideoSrc(data);
+      console.log("2", fixVideo1);
+      const res = await attachmentPlay(fixVideo1);
+      console.log(res);
+      setDataBinery(res?.data);
+
       // const getBlob = await res?.data?.blob();
       // const objectUrl = URL.createObjectURL(getBlob);
       // console.log(getBlob);
@@ -37,6 +48,11 @@ const ShowWatch = ({}) => {
     handleAttachmentListByInviteId();
   }, []);
 
+  const fixVideo1 = `${baseURL}/${videoSrc[0]?.attachmentType}/${videoSrc[0]?.fileName}${videoSrc[0]?.ext}`;
+  const fixVideo2 = `${baseURL}/${videoSrc[1]?.attachmentType}/${videoSrc[1]?.fileName}${videoSrc[1]?.ext}`;
+
+  console.log(fixVideo1);
+
   return (
     <>
       <Swiper
@@ -45,69 +61,35 @@ const ShowWatch = ({}) => {
         mousewheel={true}
         className="mySwiper h-screen"
       >
-        {videoSrc?.map((src: any, index: number) => {
-          console.log(src);
-          const fixVideo1 = `${baseURL}/${src?.[0]?.attachmentType}/${src?.[0]?.fileName}${src?.[0]?.ext}`;
-          const fixVideo2 = `${baseURL}/${src?.[1].attachmentType}/${src?.[1].fileName}${src?.[1].ext}`;
-          return (
-            <div className="grid grid-cols-1 md:grid-cols-3 justify-center  bg-white">
-              <div className="h-screen">
-                <div className="grid grid-cols-1 md:grid-cols-3 justify-center bg-white h-[80%]">
-                  <SwiperSlide key={index} className="bg-black flex flex-col">
-                    {/* <OptionHomes result profile={profile} /> */}
-                    <div className="w-[100%] h-[31vh]">
-                      <Video
-                        loop
-                        handleVideo={handleVideoTop}
-                        url={fixVideo1}
-                        playing={isPlayingTop}
-                      />
-                    </div>
-                    {/* <OptionHomes profile={profile} /> */}
-                    <div className="w-[100%] h-[31vh]">
-                      <Video
-                        loop
-                        handleVideo={handleVideoBottom}
-                        url={fixVideo2}
-                        playing={isPlayingBottom}
-                      />
-                    </div>
-                  </SwiperSlide>
+        <div className="grid grid-cols-1 md:grid-cols-3 justify-center  bg-white">
+          <div className="h-screen">
+            <div className="grid grid-cols-1 md:grid-cols-3 justify-center bg-white h-[80%]">
+              <SwiperSlide className="bg-black flex flex-col">
+                {/* <OptionHomes result profile={profile} /> */}
+                <div className="w-[100%] h-[31vh]">
+                  <Video
+                    loop
+                    handleVideo={handleVideoTop}
+                    url={dataBinery}
+                    playing={isPlayingTop}
+                  />
                 </div>
-              </div>
+                {/* <OptionHomes profile={profile} /> */}
+                <div className="w-[100%] h-[31vh]">
+                  <Video
+                    loop
+                    handleVideo={handleVideoBottom}
+                    url={fixVideo2}
+                    playing={isPlayingBottom}
+                  />
+                </div>
+              </SwiperSlide>
             </div>
-          );
-        })}
+          </div>
+        </div>
       </Swiper>
     </>
   );
 };
 
 export default ShowWatch;
-{
-  /* 
-//   <script>
-//   // Fetch the video list from the API
-//   fetch("http://localhost:5000/api/video") // Adjust the port as necessary
-//       .then(response => response.json())
-//       .then(videos => {
-//           const videoList = document.getElementById("videoList");
-//           videos.forEach(video => {
-//               const listItem = document.createElement("li");
-//               listItem.innerHTML = `<button onclick="playVideo(${video.id})">${video.title}</button>`;
-//               videoList.appendChild(listItem);
-//           });
-//       });
-
-//   // Function to play the selected video
-//   function playVideo(videoId) {
-//       const videoPlayer = document.getElementById("videoPlayer");
-//       const videoSource = document.getElementById("videoSource");
-
-//       // Update the source of the video player to the selected video
-//       videoSource.src = `http://localhost:5000/api/video/play/${videoId}`; // Adjust the URL to match your API
-//       videoPlayer.load(); // Load the new video
-//       videoPlayer.play(); // Automatically play the video
-//   }
-// </script> */
-}
