5fd49e781eb9194f67f028d6abeac8fde0aeca40
rouzbeh ghanavatiyan <armangh131313@gmail.com>
2025-12-29 09:46:04 +0330

    fix: profile base url

--- frontend/src/utils/helpers/StringHelper.tsx
+++ frontend/src/utils/helpers/StringHelper.tsx
@@ -1,5 +1,5 @@
 export default class StringHelpers {
-  static baseURL: string | undefined = import.meta.env.VITE_SERVERTEST;
+  static baseURL: string | undefined = import.meta.env.VITE_SERVERPROFILE;
  
   static getProfile = (data: any) => { 
     return `${StringHelpers.baseURL}/${data?.attachmentType}/${data?.fileName}${data?.ext}`;
